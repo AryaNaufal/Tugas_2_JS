d8a2cb8d68f8601aae7ce6c391ffb5821599731e
Arya Naufal <arya.naufal.andt@gmail.com>
2022-01-01 01:06:46 +0700

    TUGAS 2 JS

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+let nomor = 1;
+let str = "hallo world";
+let bolean = true;
+let kosong = null;
+let kosong2;
+
+
+console.log(nomor);
+console.log(str);
+console.log(bolean);
+console.log(kosong);
+console.log(kosong2);
